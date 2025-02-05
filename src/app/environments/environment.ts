e9e3e43cd92a51178df28acae1c0723ebfa3a7a0
Nzogang Nzomo Rodrigue <r.nzomo.tmrcomputing@gmail.com>
2025-02-05 21:19:09 +0100

    osus module

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    apiUrl: "http://localhost:5000/api",
+    dbConfig: {
+      host: "db",
+      user: "rooter",
+      password: "Local123!",
+      database: "solidarityconnect"
+    }
+  };
+  
